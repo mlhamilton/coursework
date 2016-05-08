74d50509fb56acf657acf905c5262d9af577fbed
Mark H <marklawrencehamilton@gmail.com>
2016-05-08 00:24:20 -0700

    Proj 4 - changing things around

--- mhamilton_jsp_classwork/productMain4/src/java/music/data/DBUtil.java
+++ mhamilton_jsp_classwork/productMain4/src/java/music/data/DBUtil.java
@@ -1,36 +1,13 @@
 package music.data;
 
-import java.sql.*;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class DBUtil {
-
-    public static void closeStatement(Statement s) {
-        try {
-            if (s != null) {
-                s.close();
-            }
-        } catch (SQLException e) {
-            System.err.println(e);
-        }
-    }
-
-    public static void closePreparedStatement(Statement ps) {
-        try {
-            if (ps != null) {
-                ps.close();
-            }
-        } catch (SQLException e) {
-            System.err.println(e);
-        }
-    }
-
-    public static void closeResultSet(ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-        } catch (SQLException e) {
-            System.err.println(e);
-        }
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("musicStorePU");
+    
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
     }
 }
